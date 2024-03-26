44050f54a3ad50736b9b4a920626483271774eb1
humphrey <writernic89@gmail.com>
2024-03-26 11:10:54 +0300

    add arrow function

---
+++ index.js
@@ -0,0 +1,18 @@
+// index.js
+
+//function divide
+function divide(h, n) {
+    return h / n;
+}
+
+//square arrow function
+const square = (num) => {
+    return num * num;
+};
+
+//function add
+const add = (h, n) => {
+    return h + n;
+};
+
+
